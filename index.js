34d1acd195251b38b33f6ad38d5ab04a0e4966ee
Nicholas Echevarria <nick.echev@gmail.com>
2017-11-28 01:32:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log string.toLowerCase()
+}
+function sayHiToGrandma() {
+  return "I can't hear you"
+  if string === .toLowerCase() 
+}
